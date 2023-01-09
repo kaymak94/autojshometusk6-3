7b0331272358430d20c0d4ce5c1958881f933173
Каймакчи Александр <kaymakchiao@mail.ru>
2023-01-09 20:18:50 +0300

    third commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 800,
+    viewportWidth: 900,
+    retries: 1,
+    baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
