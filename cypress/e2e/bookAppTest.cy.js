4135b082e5259cfe46f1b142861b8e772e06a1d3
Каймакчи Александр <kaymakchiao@mail.ru>
2023-01-12 21:41:02 +0300

    AppTestFix commit

--- cypress/e2e/bookAppTest.cy.js
+++ cypress/e2e/bookAppTest.cy.js
@@ -4,16 +4,16 @@ beforeEach(() => {
 });
 
 it("Should add books", () => {
-  cy.addBook("Каштанка, рассказ, А.П.Чехов");
-  cy.contains("Каштанка, рассказ, А.П.Чехов").should("be.visible");
+  cy.addBook("Каштанка", "рассказ", "А.П.Чехов");
+  cy.contains("Каштанка", "рассказ", "А.П.Чехов").should("be.visible");
 });
 
 it("Should add to favorite books", () => {
-  cy.addBookToFavorite("Каштанка, рассказ, А.П.Чехов");
-  cy.contains("Каштанка, рассказ, А.П.Чехов").should("be.visible");
+  cy.addBookToFavorite("Каштанка", "рассказ", "А.П.Чехов");
+  cy.contains("Каштанка", "рассказ", "А.П.Чехов").should("be.visible");
 });
 
 it("Should delete book from favorite books", () => {
   cy.get("h4").click();
-  cy.deleteBook("Каштанка, рассказ, А.П.Чехов");
+  cy.deleteBook("Каштанка", "рассказ", "А.П.Чехов");
 });
